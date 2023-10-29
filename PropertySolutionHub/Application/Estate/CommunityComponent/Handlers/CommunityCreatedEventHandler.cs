0d27a403efaba073685257b76111a4f169dc206a
vivebhagat <visual.vivek@hotmail.com>
2023-10-30 09:13:55 +1100

    Notification Handler for published notifications

---
+++ PropertySolutionHub/Application/Estate/CommunityComponent/Handlers/CommunityCreatedEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using CommunitySolutionHub.Domain.Repository.Estate;
+using MediatR;
+using Newtonsoft.Json;
+using PropertySolutionHub.Application.Estate.CommunityComponent.Notification;
+
+namespace PropertySolutionHub.Application.Estate.CommunityComponent.Handlers
+{
+    public class CommunityCreatedEventHandler: INotificationHandler<CommunityCreatedEvent>
+	{
+        private CommunityRepository _communityRepository;
+        public CommunityCreatedEventHandler(CommunityRepository communityRepository)
+		{
+            this._communityRepository = communityRepository;
+        }
+
+        public async Task Handle(CommunityCreatedEvent request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                string postData = JsonConvert.SerializeObject(request);
+                await _communityRepository.UpdateRemoteId(postData, request.localId);
+                return;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error publishing community: " + ex.Message);
+            }
+        }        
+    }
+}
+
---
+++ PropertySolutionHub/Application/Estate/CommunityComponent/Notification/CummunityCreatedEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MediatR;
+using PropertySolutionHub.Domain.Entities.Estate;
+
+namespace PropertySolutionHub.Application.Estate.CommunityComponent.Notification
+{
+	public class CommunityCreatedEvent : INotification
+	{
+        public int localId { get; internal set; }
+        public Community community { get; internal set; }
+
+        public CommunityCreatedEvent(Community community, int localId)
+		{
+			this.community = community;
+			this.localId = localId;
+		}
+    }
+}
+
